6813eff99aae12746389377a2620ec51a746ac2f
hyeleenotharry <89892255+hyeleenotharry@users.noreply.github.com>
2023-05-26 00:09:04 +0900

    Add files via upload

--- hospital/login.php
+++ hospital/login.php
@@ -1,56 +1,67 @@
+
 <?php
-// MySQL 데이터베이스 연결 정보
+// Database connection details
 $servername = "localhost";
-$username = "root";
-$password = "dlgpfl@1029";
-$dbname = "sys";
+$username = "web";
+$password = "web_admin";
+$dbname = "company";
 
-// POST로 전달된 사용자 입력 값
-$enteredId = $_POST['id'];
-$enteredPassword = $_POST['password'];
-
-// MySQL 연결
+// Establish the database connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// 연결 확인
+// Check the connection
 if ($conn->connect_error) {
-    die("MySQL 연결 실패: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
-// 입력받은 ID로 사용자 조회
-$sql = "SELECT * FROM users WHERE id = '$enteredId'";
-$result = $conn->query($sql);
+// Check if the login form is submitted
+if (isset($_POST['submit'])) {
+    // Get the entered username and password from the form
+    $username = intval($_POST['username']);
+    $password = intval($_POST['password']);
 
-// 사용자 확인
-if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    $storedPassword = $row['password'];
+    // Prepare the SQL statement
+    $sql = "SELECT * FROM usrs WHERE id = $username AND password = $password";
 
-    // 입력받은 비밀번호와 저장된 비밀번호 비교
-    if ($enteredPassword === $storedPassword) {
-        echo "로그인 성공";
-        // 로그인 성공한 경우 추가적인 작업 수행
-        $firstDigit = substr($enteredId, 0, 1);
-        if ($firstDigit === '1') {
-            // Doctor 화면으로 이동
+    // Execute the query
+    $result = $conn->query($sql);
+
+    // Check if a matching user is found
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        $id = $row['ID'];
+
+        // Redirect based on the user's ID
+        if ($id >= 10000 && $id < 20000) {
             header("Location: routes/doctor_page.php");
             exit();
-        } elseif ($firstDigit === '2') {
-            // Nurse 화면으로 이동
+        } elseif ($id >= 20000 && $id < 30000) {
             header("Location: routes/nurse_page.php");
             exit();
         } else {
-            // 기타 화면으로 이동
-            header("Location: other_page.php");
-            exit();
+            echo "Invalid user type!";
         }
     } else {
-        echo "비밀번호가 일치하지 않습니다.";
+        echo "Invalid username or password!";
     }
-} else {
-    echo "사용자가 존재하지 않습니다.";
 }
-
-// MySQL 연결 종료
-$conn->close();
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form method="post" action="">
+        <label for="username">Username:</label>
+        <input type="number" name="username" id="username" required><br>
+
+        <label for="password">Password:</label>
+        <input type="number" name="password" id="password" required><br>
+
+        <input type="submit" name="submit" value="Login">
+    </form>
+</body>
+</html>
